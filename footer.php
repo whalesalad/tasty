79c5e5df054a518ea6a6d97190d47ed14e69a27f
Michael Whalen <michael@whalesalad.com>
2009-06-06 03:58:19 -1000

    Completed the postMeta header for posts, added a footer. Established some of site-wide fonts.

--- footer.php
+++ footer.php
@@ -7,5 +7,8 @@
 <?php wp_footer(); ?>
         <div class="innerBottom"></div>
     </div>
+    <div id="footer">
+        <p>Designed by <a href="http://whalesalad.com">Michael Whalen</a> <span class="amp">&amp;</span> Powered by <a href="http://wordpress.org">WordPress</a></p>
+    </div>
 </body>
 </html>
