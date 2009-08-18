68ca4efc126f2372154212755826dd23dca11043
Michael Whalen <michael@whalesalad.com>
2009-08-17 16:35:07 -1000

    Updated version number

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * @subpackage Tasty
  */
 
-define('TASTY_VERSION', 1.1);
+define('TASTY_VERSION', 1.2);
 
 define('TASTY_LIB', TEMPLATEPATH.'/lib');
 define('TASTY_STATIC', get_bloginfo('template_url').'/lib/static');
