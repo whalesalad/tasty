2c8dc2f4486944f854701fda862987d648e16558
Michael Whalen <michael@whalesalad.com>
2009-11-29 22:34:06 -1000

    Modified changelog and screenshot for release

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
         <div class="innerBottom"></div>
     </div>
     <div id="footer">
-        <p><a href="http://whalesalad.com/tasty/">Tasty</a> by <a href="http://whalesalad.com">Michael Whalen</a> <span class="amp">&amp;</span> Powered by <a href="http://wordpress.org">WordPress</a></p>
+        <p>Running <a href="http://whalesalad.com/tasty/">Tasty</a> by <a href="http://whalesalad.com">Michael Whalen</a> <span class="amp">&amp;</span> Powered by <a href="http://wordpress.org">WordPress</a></p>
     </div>
 </body>
 </html>
