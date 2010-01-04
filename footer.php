0d7e8da6cb0659206a793e5c7eacb7b3d9a95ab1
Michael Whalen <michael@whalesalad.com>
2010-01-04 11:47:41 -1000

    Some modifications to the marrsattacks.com theme... using git to do some upgrades

--- footer.php
+++ footer.php
@@ -7,8 +7,6 @@
 <?php wp_footer(); ?>
         <div class="innerBottom"></div>
     </div>
-    <div id="footer">
-        <p>Running <a href="http://whalesalad.com/tasty/">Tasty</a> by <a href="http://whalesalad.com">Michael Whalen</a> <span class="amp">&amp;</span> Powered by <a href="http://wordpress.org">WordPress</a></p>
-    </div>
+    <div id="footer">@2009 Marrs Entertainment.  All Rights Reserved.</div>
 </body>
 </html>
--- single.php
+++ single.php
@@ -13,7 +13,8 @@ tasty_header(); ?>
                 <h2 class="<?php tasty_variable_title(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
                 <small>Posted at <?php the_time('g:i A') ?> &bull; <a href="#comments"><?php comments_number('No Comments', 'One Comment', '% Comments' );?></a><?php if ($user_ID) { ?> &bull; <?php edit_post_link('Edit Post'); ?><?php } ?></small>
             </div>
-            
+    
+
             <div class="postContent">
                 <?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?>
                 <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
@@ -22,6 +23,19 @@ tasty_header(); ?>
             </div>
         </div>
         
+<!-- AddThis Button BEGIN -->
+<?php echo "<div class=\"addthis_toolbox addthis_default_style\" addthis:url=\"".get_permalink()."\" addthis:title=\"".htmlspecialchars(get_the_title($id))."\"><a href=\"http://www.addthis.com/bookmark.php?v=250&amp;pub=marrsattacks\" class=\"addthis_button_compact\">Share</a>
+<span class=\"addthis_separator\">|</span>
+<a class=\"addthis_button_facebook\"></a>
+<a class=\"addthis_button_myspace\"></a>
+<a class=\"addthis_button_google\"></a>
+<a class=\"addthis_button_twitter\"></a>
+</div>
+<script type=\"text/javascript\" src=\"http://s7.addthis.com/js/250/addthis_widget.js#pub=marrsattacks\"></script>"; ?>
+<!-- AddThis Button END -->
+
+
+
     <?php comments_template(); ?>
     <?php endwhile; else: ?>
         <p>Sorry, no posts matched your criteria.</p>
